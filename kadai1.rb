0c82f58c4b8cd33c340abc5776132312bc1fb958
takenoko17 <j1817@matsue-ct.ac.jp>
2022-01-18 17:42:26 +0900

    2022-01-18 modified by takeshita

--- kadai1.rb
+++ kadai1.rb
@@ -1,6 +1,11 @@
 for i in 1..30
 
-	if i % 5 == 0 and i & 3 == 0
-		puts "FizzBazz!"
-	elseif i % 3 == 0
-		puts "Fizz!"
+  if i % 5 == 0 and i & 3 == 0
+    puts "FizzBazz!"
+  else if i % 3 == 0
+    puts "Fizz!"
+  else if i % 5 == 0
+    puts "Bazz!"
+  else
+    puts i
+
